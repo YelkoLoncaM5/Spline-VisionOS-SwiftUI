2ebee617d0ea978a138b814c96bcaf6590a1fca2
Yelko Loncarich <yelkoloncarichm@gmail.com>
2024-04-07 19:36:53 -0500

    Initial Commit

---
+++ SplineVisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SplineVisionOS
+//
+//  Created by Yelko Andrej Loncarich Manrique on 7/04/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SplineVisionOS/SplineVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineVisionOSApp.swift
+//  SplineVisionOS
+//
+//  Created by Yelko Andrej Loncarich Manrique on 7/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SplineVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
